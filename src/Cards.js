f97ec56cf84c618f4b097e259c5e165034db1106
“neelabhv” <neelabhv2gmail.com>
2021-05-08 11:31:12 +0530

    Code Refactor

---
+++ src/Cards.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './displaydata.css';
+
+export default function Cards({flight}){
+    const [readmore, setReadmore]=useState(false);
+    const {flight_number, mission_name, launch_success, launch_year, details} = flight;
+    const site_name = flight.launch_site.site_name;
+    const land_success = flight.rocket.first_stage.cores[0].land_success;
+    const video_link = flight.links.video_link;
+
+    return(
+        <div>
+            <div id="image">
+            <img src={flight.links.mission_patch_small} alt="Picture cannot be displayed LOLWA" />
+            </div>
+            <div id="flight-data-items">
+                <div id="mission-name">{mission_name} #{flight_number}</div>
+            <div class="flight-det"><b>Successful Launch : </b>{launch_success ? "True" : "False"} </div>
+            <div class="flight-det"><b>Launch Year : </b>{launch_year}</div>
+            <div class="flight-det"><b>Launch Site Name : </b>{site_name}</div> 
+            <div class="flight-det"><b>Details : </b>
+                {details!==null ? 
+                    <div class="expand-div">
+                    {readmore===false ? `${details}`.substring(0,20) : `${details}`}
+                    <button class="expand" onClick={(e)=>setReadmore(!readmore)}>{readmore? 'read less':'read more'}
+                    </button>
+                    </div> : "Not Available"
+                }
+            </div>
+            <div class="flight-det"><b>Youtube : </b> <a href={video_link}>click here</a></div>
+            <div class="flight-det"><b>Successful Landing : </b>{land_success ? "True" : "False"}</div>
+            
+            </div>
+        </div>
+    )
+}
--- src/DisplayData.js
+++ src/DisplayData.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 import querystring from "querystring";
+import Cards from './Cards';
 import "./displaydata.css";
 
 let BASE_URL = "https://api.spacexdata.com/v3/launches?limit=100&";
@@ -47,9 +48,7 @@ export default function DisplayData(props) {
   };
   const renderYear = () =>
     time.map((yr) => (
-        // <a href="#" onClick={(e)=>{e.stopPropagation();updateFilter("launch_year", e.target.value)}}>{yr}</a>
-        // <Link to="#" onClick={(e)=>updateFilter("launch_year", e.target.value)}></Link>
-      <button
+       <button
         id="b_year"
         value={yr}
         onClick={(e) => updateFilter("launch_year", e.target.value)}
@@ -59,13 +58,10 @@ export default function DisplayData(props) {
     ));
 
   useEffect(() => {
-    //   console.log(filters);
     axios
       .get(BASE_URL)
       .then((res) => {
-        // console.log(res);
         setApiData(res.data);
-        // window.history.pushState(null,null,'/homepage');
       })
       .catch((error) => {
         console.log(error);
@@ -88,15 +84,12 @@ export default function DisplayData(props) {
       <Fragment>
     <div id="page-head">SpaceX Launch Programs</div>
     <div id="flights">
-        
       <aside id="sidebar">
           <div id="title">
       Filters</div>
       <div class="heading">Launch Year</div>
         <div id="year">{renderYear()}</div>
         <div id="launch">
-            
-          
           <div class="heading">Successful Launch</div>
           <div class="btn">
           <button
@@ -138,19 +131,7 @@ export default function DisplayData(props) {
       <div id="apidata">
         {apiData.map((flight) => (
           <div id="flightdata">
-              <div id="image">
-            <img src={flight.links.mission_patch_small} alt="Picture cannot be displayed LOLWA" />
-            </div>
-            <div id="flight-data-items">
-                <div id="mission-name">{flight.mission_name} #{flight.flight_number}</div>
-            <div class="flight-det"><b>Successful Launch : </b>{flight.launch_success ? "True" : "False"} </div>
-            <div class="flight-det"><b>Launch Year : </b>{flight.launch_year}</div>
-            <div class="flight-det"><b>Launch Site Name : </b>{flight.launch_site.site_name}</div>
-            <div class="flight-det"><b>Details : </b>{flight.details === null ? "Not Available" : `${flight.details}`}</div>
-            <div class="flight-det"><b>Youtube : </b> <a href={flight.links.video_link}>click here</a></div>
-            <div class="flight-det"><b>Successful Landing : </b>{flight.rocket.first_stage.cores[0].land_success ? "True" : "False"}</div>
-            
-            </div>
+              <Cards flight={flight}/>
           </div>
         ))}
       </div>
